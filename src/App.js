210ba587e98bcc78cf6dd9d383ab10be6f72afa3
Justin HB <jbruell94@gmail.com>
2024-01-07 17:23:07 +0700

    Changed Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import SBOM from './pages/SBOM';
 import About from './pages/About';
 import Navigation from './components/Navigation';
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,10 +6,7 @@ function Navigation() {
     <nav className="navbar fixed-top bg-body-tertiary">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">SBOM Viewer</Link>
-        <div>
-          <Link to="/about">About</Link>
-          <Link to="/sbom">SBOM</Link>
-        </div>
+        <Link className="navbar-brand" to="/about">About</Link>
       </div>
     </nav>
   );
